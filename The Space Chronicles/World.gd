583dcda81011b33f0babd812301a7df430ce1026
SleepiiRaven <sleepingravenytb@gmail.com>
2022-06-07 18:09:33 -0700

    Enemy Label Updates Faster

--- The Space Chronicles/World.gd
+++ The Space Chronicles/World.gd
@@ -3,5 +3,9 @@ extends Node2D
 var enemy = 20
 
 func _physics_process(delta):
-	enemy = get_tree().get_nodes_in_group("enemy").size()
+	var enemy_list = get_tree().get_nodes_in_group("enemy")
+	enemy = enemy_list.size()
+	for i in enemy:
+		if enemy_list[i].get_node("Sprite").visible == false:
+			enemy -= 1
 	$CanvasLayer/GameScore.text = ("There are currently " + str(enemy) + " enemies left")
