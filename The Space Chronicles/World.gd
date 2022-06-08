28e7f396b420bcf132b1cbe3b382e56a352ffe63
SleepiiRaven <sleepingravenytb@gmail.com>
2022-06-07 17:29:20 -0700

    Update World.gd

--- The Space Chronicles/World.gd
+++ The Space Chronicles/World.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 var enemy = 20
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	enemy = get_tree().get_nodes_in_group("enemy").size()
 	$CanvasLayer/GameScore.text = ("There are currently " + str(enemy) + " enemies left")
